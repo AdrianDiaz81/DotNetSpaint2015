3bf4e5fc9057702295f873d98a5f8cb0cd638649
AdrianDiaz81 <adiaz@encamina.com>
2015-03-03 07:46:25 +0100

    Variable no definida

--- Contacts.Cordova/scripts/js/contact-list-ctrl.js
+++ Contacts.Cordova/scripts/js/contact-list-ctrl.js
@@ -1,22 +1,19 @@
 ﻿(function () {
     'use strict';
-
     angular.module('app365').controller('contactCtrl', ['$scope', 'app365api', contactCtrl]);
-
     function contactCtrl($scope, app365api) {
         var vm = this;
         var outlookClient;
 
         function getContacts() {
-            NProgress.start();
-
+           
             // Fetch all the contacts.
             outlookClient.me.contacts.getContacts().fetch()
             .then(function (contacts) {
                 // Get the current page. Use getNextPage() to fetch next set of contacts.
                 vm.contacts = contacts.currentPage;
                 $scope.$apply();
-                NProgress.done();
+                
             });
         };
 
